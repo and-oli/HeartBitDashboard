98912700e0442b8f42296f48f47dde8ac2a71a6b
Andrés <af.olivares10@uniandes.edu.co>
2018-05-04 01:56:17 -0500

    Add C map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import * as d3 from "d3";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,19 +10,65 @@ class App extends Component {
   }
   componentDidMount ()
   {
+
+
+
+
     fetch('/api/')
     .then((response) => response.json())
     .then((responseJson) => {
-      console.log(responseJson.data);
       if(responseJson.success){
         this.setState({mierda:responseJson.data});
       }
       else{
-        console.log(responseJson);
       }
     })
     .catch((error) => {
       console.error(error);
+    });
+    var width = 500;
+    var height = 500;
+
+    var projection = d3.geoMercator()
+    .translate([1700, 300])
+    .scale([1200]);
+
+    var path = d3.geoPath()
+    .projection(projection);
+    var culo = d3.select("culo").text("culito");
+    var mapa = d3.select("div#mapa")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g");
+
+    var div = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+    d3.select("div.tooltip")
+    .append("h3");
+
+    d3.json("./static/dptos.json").then( function(json){
+      mapa.selectAll("path")
+      .data(json.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("stroke", "#fff")
+      .style("fill", "#ccc")
+      .style("stroke-width", "1.5")
+
+      .on("mouseover", function(d){
+        div.transition()
+        .duration(200)
+        .style("opacity", 0.9);
+        div.select("h3")
+        .text(d.properties["NOMBRE_DPT"]);
+        div.style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 30) + "px");
+      })
     });
   }
   mostrarHistorial = () =>{
@@ -52,7 +97,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div>culo</div>
+        <div id="mapa"></div>
+        <h1 id="culo"></h1>
+
         <div className="modal-body">
           {this.mostrarHistorial()}
         </div>
